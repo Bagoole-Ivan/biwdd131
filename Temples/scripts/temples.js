b64ed1d28d5cf4b0bebb83d9e03517cb5118cdf2
Bagoole Ivan <bagooleivan2001@gmail.com>
2024-11-16 12:53:07 +0300

    nav change in temples

--- Temples/scripts/temples.js
+++ Temples/scripts/temples.js
@@ -1,13 +1,26 @@
-
+const hamburger = document.querySelector('.topnav');
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('hide');
+    });
 
 function formatDate(date) {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
     return date.toLocaleDateString('en-US', options);
 }
 
+
 // Getting the current date
 const currentDate = new Date();
 
 // Inserting the formatted date into the HTML element with the id "current-date"
 document.getElementById('current-date').textContent = formatDate(currentDate);
+
+
+//const hamburger = document.querySelector('.hamburger-menu');
+//const navMenu = document.querySelector('.nav-menu');
+    
+//hamburger.addEventListener('click', () => {
+  //navMenu.classList.toggle('hide');
+//});
+
 
